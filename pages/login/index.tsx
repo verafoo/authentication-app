d0ae85d159b2b7b438f35e7fe7e6e60256059f72
verafoo <492226508@qq.com>
2021-11-06 20:11:18 +0800

    some of the login styles

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,9 +2,10 @@ import type { NextPage } from "next";
 import { Card, Form, Input, Button, Checkbox } from "antd";
 import Image from "next/image";
 import { useState } from "react";
+import styles from "../../styles/login.module.css";
 import {
-  UserOutlined,
-  MailOutlined,
+  MailFilled,
+  LockFilled,
   GoogleOutlined,
   FacebookOutlined,
   TwitterOutlined,
@@ -13,7 +14,7 @@ import {
 const devchallengesSvg = require("../../public/devchallenges.svg");
 
 const Login: NextPage = () => {
-  const [isRegister, setIsRegister] = useState(false);
+  const [isRegister, setIsRegister] = useState(true);
   const onFinish = (values: any) => {
     console.log("Success:", values);
   };
@@ -22,94 +23,97 @@ const Login: NextPage = () => {
     console.log("Failed:", errorInfo);
   };
   return (
-    <>
-      <Card style={{ width: 300 }}>
-        <div className="info">
-          {isRegister ? (
-            <>
-              <Image src={devchallengesSvg} alt="devchallenges" />
-              <p>Join thousands of learners from around the world</p>
+    <div className={styles["login-main-content"]}>
+      <Card className={styles["login-card"]}>
+        <div className={styles["login-card-content"]}>
+          <div className={styles["login-card-info"]}>
+            {isRegister ? (
+              <>
+                <Image src={devchallengesSvg} alt="devchallenges" />
+                <p>Join thousands of learners from around the world</p>
+                <p>
+                  Master web development by making real-life projects. There are
+                  multiple paths for you to choose
+                </p>
+              </>
+            ) : (
+              <p>Login</p>
+            )}
+          </div>
+
+          <div className={styles["login-card-register-form"]}>
+            <Form
+              name="basic"
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item
+                name="email"
+                rules={[
+                  { required: true, message: "Please input your email!" },
+                ]}
+              >
+                <Input
+                  placeholder="email"
+                  prefix={<MailFilled className="site-form-item-icon" />}
+                />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your password!" },
+                ]}
+              >
+                <Input.Password
+                  placeholder="password"
+                  prefix={<LockFilled className="site-form-item-icon" />}
+                />
+              </Form.Item>
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  {isRegister ? "Start coding now" : "Login"}
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+
+          <div className="login">
+            <p>or continue with these social profile</p>
+            <div className="circle-icon">
+              <GoogleOutlined />
+            </div>
+            <div className="circle-icon">
+              <FacebookOutlined />
+            </div>
+            <div className="circle-icon">
+              <TwitterOutlined />
+            </div>
+            <div className="circle-icon">
+              <GithubOutlined />
+            </div>
+
+            {isRegister ? (
               <p>
-                Master web development by making real-life projects. There are
-                multiple paths for you to choose
+                Adready a member? <a href="#">Login</a>
               </p>
-            </>
-          ) : (
-            <p>Login</p>
-          )}
-        </div>
-
-        <div className="register-form">
-          <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-            <Form.Item
-              name="email"
-              rules={[{ required: true, message: "Please input your email!" }]}
-            >
-              <Input
-                placeholder="email"
-                prefix={<MailOutlined className="site-form-item-icon" />}
-              />
-            </Form.Item>
-
-            <Form.Item
-              name="password"
-              rules={[
-                { required: true, message: "Please input your password!" },
-              ]}
-            >
-              <Input.Password
-                placeholder="password"
-                prefix={<UserOutlined className="site-form-item-icon" />}
-              />
-            </Form.Item>
-
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                {isRegister ? "Start coding now" : "Login"}
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
-
-        <div className="login">
-          <p>or continue with these social profile</p>
-          <div className="circle-icon">
-            <GoogleOutlined />
+            ) : (
+              <p>
+                Don’t have an account yet? <a href="#">Register</a>
+              </p>
+            )}
           </div>
-          <div className="circle-icon">
-            <FacebookOutlined />
-          </div>
-          <div className="circle-icon">
-            <TwitterOutlined />
-          </div>
-          <div className="circle-icon">
-            <GithubOutlined />
-          </div>
-
-          {isRegister ? (
-            <p>
-              Adready a member? <a href="#">Login</a>
-            </p>
-          ) : (
-            <p>
-              Don’t have an account yet? <a href="#">Register</a>
-            </p>
-          )}
         </div>
       </Card>
-      <div className="creator-info">
-        <p>created by verafoo</p>
-        <p>devChallenges.io</p>
+      <div className="login-creator-info">
+        <p>
+          <span>created by verafoo</span>
+          <span>devChallenges.io</span>
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 
